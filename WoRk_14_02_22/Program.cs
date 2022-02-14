710b2eaf5c0676dbd1754f6361df82ab46997207
XoLoDiLniC <99400502+XoLoDiLniC@users.noreply.github.com>
2022-02-14 16:10:07 +0300

    XoLoDiLnIk
    Исправлена ошибка в Program.cs
    The Main() method

--- WoRk_14_02_22/Program.cs
+++ WoRk_14_02_22/Program.cs
@@ -10,29 +10,14 @@ namespace WoRk_14_02_22
     {
         static void Main(string[] args)
         {
-            static IEnumerable<string> Suits()
-            {
-                yield return "clubs";
-                yield return "diamonds";
-                yield return "hearts";
-                yield return "spades";
-            }
+            var startingDeck = from s in Suits()
+                               from r in Ranks()
+                               select new { Suit = s, Rank = r };
 
-            static IEnumerable<string> Ranks()
+            // Display each card that we've generated and placed in startingDeck in the console
+            foreach (var card in startingDeck)
             {
-                yield return "two";
-                yield return "three";
-                yield return "four";
-                yield return "five";
-                yield return "six";
-                yield return "seven";
-                yield return "eight";
-                yield return "nine";
-                yield return "ten";
-                yield return "jack";
-                yield return "queen";
-                yield return "king";
-                yield return "ace";
+                Console.WriteLine(card);
             }
         }
     }
